afc5aa7326f1ea51318b9d55f677c9d0cb6f5a4e
Kiminonawa702 <w.hatsagan@gmail.com>
2024-10-28 08:45:16 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,27 @@
+// settings.js
+
+module.exports = {
+  // Aktifkan atau nonaktifkan fitur Auto Read Status
+  autoReadStory: true, 
+
+  // Aktifkan atau nonaktifkan fitur Auto Typing
+  autoTyping: false,
+
+  // Aktifkan atau nonaktifkan fitur Update Bio (Aktif Selama Uptime)
+  bioActive: false,
+
+  // Aktifkan atau nonaktifkan fitur Delete Status
+  deleteStatus: false, 
+
+  // Kecepatan melihat status (dalam milidetik, minimal 1 detik)
+  readStatusSpeed: 500, // 2 detik
+
+  // Aktifkan atau nonaktifkan tampilan statistik status saat bot dimulai
+  showStatistics: true,
+
+  // Aktifkan atau nonaktifkan tampilan total view status 
+  showViewStatus: false,
+
+  // Aktifkan atau nonaktifkan tampilan total delete status
+  showDeleteStatus: false 
+};
